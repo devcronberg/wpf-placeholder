a75b869757a2c2500f4782728ec0f8005a9b708b
Michell Cronberg <michell@cronberg.dk>
2025-02-17 20:12:20 +0100

    Add initial WPF application structure with main window and buttons

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WpfTest;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private static readonly HttpClient client = new HttpClient();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        Button1.Click += Button1_Click;
+        Button2.Click += Button2_Click;
+        Button3.Click += Button3_Click;
+    }
+
+    private async void Button1_Click(object sender, RoutedEventArgs e)
+    {
+        await Task.Delay(100);
+    }
+
+
+    private async void Button2_Click(object sender, RoutedEventArgs e)
+    {
+        await Task.Delay(100);
+    }
+
+    private async void Button3_Click(object sender, RoutedEventArgs e)
+    {
+        await Task.Delay(100);
+
+    }
+}
